bbf73b3ed4e484dde27b1048194280c224b25f33
Orion <orion45.dd@gmail.com>
2024-10-15 08:35:29 -0700

    update

--- utils/redis.js
+++ utils/redis.js
@@ -1,38 +1,39 @@
-import { createClient } from 'redis';
-
+const redis = require('redis');
+const { promisify }= require('util');
+/**
+ * Redis utils
+ */
 class RedisClient {
   constructor() {
-    this.client = createClient(); 
+    this.client = redis.createClient(); 
 
     this.client.on('error', (err) => {
       console.error('Error:', err);
     });
+    
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.set).bind(this.client);
+    this.delAsync = promisify(this.client.set).bind(this.client);
+    this.delAsync = promisify(this.client.set).bind(this.client);
   }
+
   isAlive() {
     return this.client.connected;
   }
+
   async get(key) {
-    try {
       return await this.client.get(key);
-    } catch (err) {
-        console.error(err);
-      return null;
-    }
   }
+
   async set(key, value, duration) {
-    try {
-      await this.client.set(key, value, {EX: duration});
-    } catch (err) {
-        console.error(err);
+    await this.setAsync(key, value);
+    this.client.expire(expirationInSeconds);    
     }
+
+  async del(key){
+    await this.delAsyns(key);
   }
-  async del(key) {
-    try {
-      await this.client.del(key);
-    } catch (err) {
-        console.error(err);
-    }
-  }  
 }
-const redisClient = new RedisClient();
-export default redisClient;
+
+module.exports = new RedisClient();
+module.export = redisClient;
